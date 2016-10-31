6eb56df34076e4fca1b24d31db6e88b5982e75e4
papago <kuo.sunnyfruit@gmail.com>
2016-11-01 00:27:18 +0800

    Landing page

--- SuperMan/Common/UrlBuilder.cs
+++ SuperMan/Common/UrlBuilder.cs
@@ -10,6 +10,12 @@ namespace SuperMan.Common
 
         private const string CssPath = "../Content/Css/";
 
+        private const string LandingImagePath = "../LandingPage/img/";
+
+        private const string LandingScriptPath = "../LandingPage/js/";
+
+        private const string LandingCssPath = "../LandingPage/css/";
+
         public static MvcHtmlString ImageUrl(this HtmlHelper helper, string text)
         {
             return MvcHtmlString.Create(ImagePath + text);
@@ -23,6 +29,21 @@ namespace SuperMan.Common
         public static MvcHtmlString CssUrl(this HtmlHelper helper, string text)
         {
             return MvcHtmlString.Create(CssPath + text);
+        }
+
+        public static MvcHtmlString LandingImageUrl(this HtmlHelper helper, string text)
+        {
+            return MvcHtmlString.Create(LandingImagePath + text);
+        }
+
+        public static MvcHtmlString LandingScriptUrl(this HtmlHelper helper, string text)
+        {
+            return MvcHtmlString.Create(LandingScriptPath + text);
+        }
+
+        public static MvcHtmlString LandingCssUrl(this HtmlHelper helper, string text)
+        {
+            return MvcHtmlString.Create(LandingCssPath + text);
         }
     }
 }
